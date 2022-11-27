e4bdba996807c4cca3925317b53e00bae33f4a16
razieltx <deyberbejarano97@gmail.com>
2022-11-27 18:15:32 -0500

    Set up the routes of the page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
+import indexRouter from './routes/index.js';
+
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => res.render('index'));
+app.use(indexRouter);
 
 app.listen(3000);
 console.log('Server is listening on port', 3000);
